fadbc3ea7c498faa0597d45358c683d7bf7bc636
sjkdhdkzjhdjkqwhkdjashjkdsahkew <77858656+sjkdhdkzjhdjkqwhkdjashjkdsahkew@users.noreply.github.com>
2021-01-22 15:28:22 -0500

    Add files via upload

---
+++ y or n question.py
@@ -0,0 +1,7 @@
+answer = input("would you like to delete the file? [y/n] ")
+if answer == "y":
+  print('ok, deleting file: user/bo/asd/u:drive/sdaw/a/sdwa/sda/wd... Task was completed sucsessfully')
+elif answer == "n":
+  print('ok, will not delete file')
+else:
+    print("Please enter y or n.y")
